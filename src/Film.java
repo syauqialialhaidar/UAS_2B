59f035960f21d1a5f80351425cf25f5996d34ec6
syauqialialhaidar <syauqialialhaidar@gmail.com>
2023-07-14 14:24:28 +0700

    1

--- src/Film.java
+++ src/Film.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Film {
+public class Film {
+    private int IdFilm;
+    private String NamaFilm;
+    private int Tahun;
+    private double Rating;
+
+    public Film(int idFilm, String namaFilm, int tahun, double rating) {
+        this.IdFilm = idFilm;
+        this.NamaFilm = namaFilm;
+        this.Tahun = tahun;
+        this.Rating = rating;
+    }
+
+    public int getIdFilm() {
+        return IdFilm;
+    }
+
+    public void setIdFilm(int idFilm) {
+        this.IdFilm = idFilm;
+    }
+
+    public String getNamaFilm() {
+        return NamaFilm;
+    }
+
+    public void setNamaFilm(String namaFilm) {
+        this.NamaFilm = namaFilm;
+    }
+
+    public int getTahun() {
+        return Tahun;
+    }
+
+    public void setTahun(int tahun) {
+        this.Tahun = tahun;
+    }
+
+    public double getRating() {
+        return Rating;
+    }
+
+    public void setRating(double rating) {
+        this.Rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Film{" +
+                "idFilm=" + IdFilm +
+                ", namaFilm='" + NamaFilm + '\'' +
+                ", tahun=" + Tahun +
+                ", rating=" + Rating +
+                '}';
+    }
 }
--- src/FilmTerlaris.java
+++ src/FilmTerlaris.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class FilmTerlaris {
+public class FilmTerlaris {
+    public static void main(String[] args) {
+        Film[] filmArray = new Film[5];
+        filmArray[0] = new Film(1, "The Flash", 2023, 7.3);
+        filmArray[1] = new Film(2, "Spiderman:AS-V", 2023, 9.0);
+        filmArray[2] = new Film(3, "Transformer:SOB", 2023, 6.6);
+        filmArray[3] = new Film(4, "Avatar : TOW", 2022, 7.6);
+        filmArray[4] = new Film(5, "The Little Mermaid", 2023, 7.2);
+
+        System.out.println("Daftar Film Terlaris:");
+        for (Film film : filmArray) {
+            System.out.println(film);
+        }
+
+        for (int i = 0; i < filmArray.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < filmArray.length; j++) {
+                if (filmArray[j].getRating() < filmArray[minIndex].getRating()) {
+                    minIndex = j;
+                }
+            }
+            Film temp = filmArray[minIndex];
+            filmArray[minIndex] = filmArray[i];
+            filmArray[i] = temp;
+        }
+
+
+        System.out.println("Daftar Film Terlaris (Setelah Diurutkan):");
+        for (Film film : filmArray) {
+            System.out.println(film);
+        }
+    }
 }
